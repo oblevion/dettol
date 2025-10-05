92fafbec824853eb7be8264a9f3f1cd3840687d9
oblevion <commanderp92@gmail.com>
2025-10-05 22:05:20 +0530

    Delete agentic reasoning system directory

--- agentic reasoning system/modules/parser.py
+++
@@ -1,8 +0,0 @@
-def decompose_problem(question):
-    if "if" in question and "," in question:
-        clauses = question.split(",")
-        return [clause.strip() for clause in clauses]
-    elif "and" in question and "," not in question:
-        return [clause.strip() for clause in question.split("and")]
-    else:
-        return [question]
